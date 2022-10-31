fbe931f486c44188cd7622d582130015955fc208
Yoni Limo <limzyon@gmail.com>
2022-11-01 00:20:25 +0300

    __

--- try.py
+++ try.py
@@ -1,9 +1,3 @@
-def nazi_hunter(arr):
-    n = len(arr)+1
-    tot = sum(arr)
-    missing = (n * (n+1))/2 - tot
-    return missing
-
-
-arr = [1, 2, 3, 4, 5, 6, 8]
-print(nazi_hunter(arr))
+list1 = ["afhas, aidwi", "ippow, aiiwpa"]
+for i in list1:
+    print((", ").split(i))
