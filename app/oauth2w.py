8c9a8223188b2d6993be4eb71a568b21d1b88cb7
Yoni Limo <limzyon@gmail.com>
2023-01-21 16:26:07 +0300

    added jwt auth

--- app/oauth2w.py
+++ app/oauth2w.py
@@ -1,2 +1,40 @@
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi.encoders import jsonable_encoder
+from . import utilities, schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = "01vsdty548sghk89pttes7a82vh2i2osef8hb5swrt2366lp8if2s2a5s899a4"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expiry_time = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"expiry": jsonable_encoder(expiry_time)})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if not id:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+    
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers= {"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token=token, credentials_exception=credential_exception)
