9733f14ada39b0116b3086f92deed134b5528ad3
Yoni Limo <limzyon@gmail.com>
2023-01-22 15:37:13 +0300

    __

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,7 +6,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def login(user_credetials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(models.User.email == user_credetials.username).first()
