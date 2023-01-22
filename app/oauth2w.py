2d93926c31f36fff29b90705033f2ca269ac5ff8
Yoni Limo <limzyon@gmail.com>
2023-01-22 16:03:13 +0300

    added automation on token generation

--- app/oauth2w.py
+++ app/oauth2w.py
@@ -1,10 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi.encoders import jsonable_encoder
-from . import utilities, schemas
+from . import utilities, schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -34,6 +34,9 @@ def verify_access_token(token: str, credentials_exception):
     
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers= {"WWW-Authenticate": "Bearer"}, )
-    return verify_access_token(token=token, credentials_exception=credential_exception)
+    
+    token = verify_access_token(token=token, credentials_exception=credential_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
