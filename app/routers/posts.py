7c82db2712c40582e3a7625e877cb45e4ef351c8
Yoni Limo <limzyon@gmail.com>
2023-01-22 16:03:37 +0300

    updated token authentication

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -10,32 +10,32 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.PostReturn])
-def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2w.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2w.get_current_user)):
     posts = db.query(models.Post).all()
-    print(user_id)
+    print(current_user.email)
     print(posts)
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostReturn)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2w.get_current_user)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2w.get_current_user)):
     new_post = models.Post(**post.dict())
-    print(user_id)
+    print(current_user.email)
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
 @router.get("/{id}", response_model=schemas.PostReturn)
-def get_single_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2w.get_current_user)):
+def get_single_post(id: int, db: Session = Depends(get_db), curent_user: int = Depends(oauth2w.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
-    print(user_id)
+    print(curent_user.email)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found.")
     return post
 
 
 @router.delete("/{id}")
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2w.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2w.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id {id} does not exist.")
@@ -45,7 +45,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
 
 
 @router.put("/{id}", response_model=schemas.PostReturn)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2w.get_current_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2w.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post == None:
