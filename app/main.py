a4dcf50982fa025d5eb6b84c4a95cd4cc67e09ac
Yoni Limo <limzyon@gmail.com>
2023-02-03 17:00:09 +0300

    validating config credentials

--- app/main.py
+++ app/main.py
@@ -1,32 +1,16 @@
-from typing import Optional, List
 
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from . import models, schemas, utilities
-from .database import engine, get_db
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+
+from . import models
+from .database import engine
 from .routers import posts, users, auth
+from .config import settings
+
+print(settings.database_password)
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres", password="password", cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Succesful connection.")
-        break
-    except Exception as error:
-        print("Database connection failed")
-        print(error)
-        time.sleep(2)
-
 
 app.include_router(posts.router)
 app.include_router(users.router)
