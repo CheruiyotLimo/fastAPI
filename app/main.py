58863a99ba94415d5d8ec5a0ee5a47e52fff10f5
Yoni Limo <limzyon@gmail.com>
2023-02-07 16:05:49 +0300

    included vote router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import posts, users, auth
+from .routers import posts, users, auth, vote
 from .config import settings
 
 print(settings.database_password)
@@ -15,6 +15,7 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 async def root():
