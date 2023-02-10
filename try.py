df76dd54d7fd2478099321eab263f6310c7657a2
Yoni Limo <limzyon@gmail.com>
2023-02-10 15:21:58 +0300

    best day to buy/sell stock

--- try.py
+++ try.py
@@ -1,3 +1,18 @@
-list1 = ["afhas", "aidwi", "ippow", "aiiwpa"]
+import re
 
-list1.index("aidwi")
+def maxProfit(prices: list[int]) -> int:
+        k, j = 0, 0
+        for i in range(len(prices)):
+            if prices[i] <= prices[k]:
+                k = i
+                j = i
+            
+        for x in range(j+1, len(prices)):
+            if prices[x] >= prices[j]:
+                j = x
+        
+        if prices[k] == prices[j]:
+            return 0
+        return prices[j] - prices[k]
+
+print(maxProfit(prices = [2, 1, 4, 1]))
