c697f44cda23eec357ae57b7f2f661c42c94e5ad
Yoni Limo <limzyon@gmail.com>
2023-02-14 17:05:06 +0300

    Implement a queue with two stacks

--- try.py
+++ try.py
@@ -1,18 +1,33 @@
-import re
+class MyQueue:
 
-def maxProfit(prices: list[int]) -> int:
-        k, j = 0, 0
-        for i in range(len(prices)):
-            if prices[i] <= prices[k]:
-                k = i
-                j = i
-            
-        for x in range(j+1, len(prices)):
-            if prices[x] >= prices[j]:
-                j = x
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
         
-        if prices[k] == prices[j]:
-            return 0
-        return prices[j] - prices[k]
+    def pop(self) -> int:
+        while self.stack1:
+            top = self.stack1.pop()
+            self.stack2.append(top)
+        result = self.stack2.pop()
+        while self.stack2:
+            new_top = self.stack2.pop()
+            self.stack1.append(new_top)
+        return result
 
-print(maxProfit(prices = [2, 1, 4, 1]))
+    def peek(self) -> int:
+        return self.stack1[0]
+
+    def empty(self) -> bool:
+        if not self.stack1:
+            return True
+        return False
+
+mq = MyQueue()
+mq.push(1)
+print(mq.pop())
+print(mq.stack1)
+print(mq.stack2)
+print(mq.empty())
