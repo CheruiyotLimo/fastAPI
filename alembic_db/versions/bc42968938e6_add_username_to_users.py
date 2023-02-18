dde8ac6e7eb8232222c09d841019cee2ebe58edf
Yoni Limo <limzyon@gmail.com>
2023-02-18 16:23:16 +0300

    added username to users

---
+++ alembic_db/versions/bc42968938e6_add_username_to_users.py
@@ -0,0 +1,26 @@
+"""add username to users
+
+Revision ID: bc42968938e6
+Revises: d3bc43f58293
+Create Date: 2023-02-18 16:17:12.368848
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc42968938e6'
+down_revision = 'd3bc43f58293'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("users", sa.Column("username", sa.String(), nullable=False), sa.UniqueConstraint("username"))
+
+
+def downgrade() -> None:
+    op.drop_constraint("username", "users")
+    op.drop_column("users", "username")
+    pass
