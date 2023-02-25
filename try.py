49d1df2c0c901973a592899ab3a2e823fa3ae39a
Yoni Limo <limzyon@gmail.com>
2023-02-25 22:01:51 +0300

    try

--- try.py
+++ try.py
@@ -1,9 +1,5 @@
-from collections import Counter
 
-al = "abccccdd"
-result = 0
 
-for i in Counter(al).values():
-    result += i // 2 * 2
-
-print(min(result+1, len(al)))
+asl = [21, 4, 5, 6, 723]
+for i, t in enumerate(asl):
+    print(i, t)
